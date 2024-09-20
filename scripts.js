8022805e59eb65284378ca3349bba546fd66d791
Klychnikov Alexandr <klychnikov75@mail.ru>
2024-09-20 23:33:03 +0300

    feat: create clock and date display with every second ticking

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 const container = document.getElementById('main-container');
+const clock = document.querySelector('.clock');
+const calendar = document.querySelector('.date');
 
 let timeOfBackgroundSetting;
 
@@ -16,12 +18,48 @@ function updateBackground() {
 function getTimeToChangeBackground (date) {
   const nowHour = date.getHours();
   const triggerHours = [0, 6, 12, 18, 24];
-  const hourIndex = triggerHours.reduce((a, c, i) => c < nowHour ? i : a, 0)
-  const timeToChange = new Date(date.getFullYear(), date.getMonth(), date.getDate(), triggerHours[hourIndex + 1])
+  const hourIndex = triggerHours.reduce((a, c, i) => c < nowHour ? i : a, 0);
+  const timeToChange = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    triggerHours[hourIndex + 1]
+  );
   const msToNextChange = timeToChange - date;
   return msToNextChange;
 }
 
 updateBackground();
+
+function getCurrentDateTime() {
+  const now = new Date();
+  const timeOptions = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  }
+  const dateOptions = {
+    day: 'numeric',
+    month: 'long',
+    weekday: 'long',
+  };
+    const time = now.toLocaleTimeString('ru', timeOptions);
+    const date = now.toLocaleDateString('ru', dateOptions).split(',').reverse().join(', ');
+    return {time, date};
+}
+
+const setTimeAndDate = () => {
+  const {time, date} = getCurrentDateTime()
+  clock.textContent = time;
+  calendar.textContent = date;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeAndDate()
+});
+
+setInterval(() => {
+  setTimeAndDate();
+}, 1000);
 
 
