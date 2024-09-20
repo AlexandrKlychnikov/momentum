16e44c896c34b6dc4bddcecbb503ad8668efa054
Klychnikov Alexandr <klychnikov75@mail.ru>
2024-09-20 20:55:12 +0300

    refactor: simplify code in range of time counting

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-function updateBackgroundSlider() {
-  const container = document.getElementById('main-container');
-    const times = ['night', 'morning', 'day', 'evening']
-    const now = new Date();
-    let hours = now.getHours();
-    if (hours === 0) {
-        hours = 24;
-    }
-    const timeIndex = Math.floor((hours / 6) % times.length);
-    container.className = times[timeIndex];
+const container = document.getElementById('main-container');
+
+let timeOfBackgroundSetting;
+
+function updateBackground() {
+  const times = ['night', 'morning', 'day', 'evening']
+  const now = new Date();
+  let hours = now.getHours();
+  const timeIndex = Math.floor((hours / 6));
+  container.className = times[timeIndex];
+  timeOfBackgroundSetting = now;
 }
 
-updateBackgroundSlider();
+updateBackground();
+
+
