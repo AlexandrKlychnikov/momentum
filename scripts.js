bb87605d7f7f110ce7949b90cd1840b223176369
Klychnikov Alexandr <klychnikov75@mail.ru>
2024-09-20 22:48:48 +0300

    feat: implement setting of change background accordingly with part of day

--- scripts.js
+++ scripts.js
@@ -5,10 +5,21 @@ let timeOfBackgroundSetting;
 function updateBackground() {
   const times = ['night', 'morning', 'day', 'evening']
   const now = new Date();
-  let hours = now.getHours();
+  const hours = now.getHours();
   const timeIndex = Math.floor((hours / 6));
   container.className = times[timeIndex];
   timeOfBackgroundSetting = now;
+
+  setTimeout(updateBackground, getTimeToChangeBackground(timeOfBackgroundSetting));
+}
+
+function getTimeToChangeBackground (date) {
+  const nowHour = date.getHours();
+  const triggerHours = [0, 6, 12, 18, 24];
+  const hourIndex = triggerHours.reduce((a, c, i) => c < nowHour ? i : a, 0)
+  const timeToChange = new Date(date.getFullYear(), date.getMonth(), date.getDate(), triggerHours[hourIndex + 1])
+  const msToNextChange = timeToChange - date;
+  return msToNextChange;
 }
 
 updateBackground();
